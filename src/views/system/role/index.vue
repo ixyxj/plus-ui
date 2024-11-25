6eeb711d2fb25e864b764e6433deac69e8cea3cd
疯狂的狮子Li <15040126243@163.com>
2024-11-25 10:33:47 +0800

    update 优化 增加默认数据权限 "部门及以下或本人数据权限" 选项

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -223,7 +223,8 @@ const dataScopeOptions = ref([
   { value: '2', label: '自定数据权限' },
   { value: '3', label: '本部门数据权限' },
   { value: '4', label: '本部门及以下数据权限' },
-  { value: '5', label: '仅本人数据权限' }
+  { value: '5', label: '仅本人数据权限' },
+  { value: '6', label: '部门及以下或本人数据权限' }
 ]);
 
 const queryFormRef = ref<ElFormInstance>();
