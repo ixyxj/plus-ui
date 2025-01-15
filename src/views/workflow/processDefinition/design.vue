71528c78acb8702c7de73ac0416f18642aa6e120
疯狂的狮子Li <15040126243@163.com>
2025-01-15 18:20:29 +0800

    update 优化 设计器界面展示效果

--- src/views/workflow/processDefinition/design.vue
+++ src/views/workflow/processDefinition/design.vue
@@ -1,17 +1,30 @@
 <template>
   <div ref="container" class="container">
-    <iframe ref="iframe" :src="iframeUrl" frameborder="0" width="100%" height="100%"></iframe>
+    <iframe ref="iframe" :src="iframeUrl" frameborder="0" :width="sidebarStatusWidth()" height="100%"></iframe>
   </div>
 </template>
 
 <script setup name="WarmFlow">
+
 const { proxy } = getCurrentInstance();
 import { onMounted } from 'vue';
+import { getToken } from '@/utils/auth';
+
+const sidebarStatus = useStorage('sidebarStatus', '1');
+
+console.log(sidebarStatus.value)
+
+const sidebarStatusWidth = () => {
+  if (sidebarStatus.value === '0') {
+    return "115%";
+  }
+  return "104%";
+}
+
 // definitionId为需要查询的流程定义id，
 // disabled为是否可编辑, 例如：查看的时候不可编辑，不可保存
 const iframeUrl = ref('');
 const baseUrl = import.meta.env.VITE_APP_BASE_API;
-import { getToken } from '@/utils/auth';
 const iframeLoaded = () => {
   // iframe监听组件内设计器保存事件
   window.onmessage = (event) => {
